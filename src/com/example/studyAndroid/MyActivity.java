a4f8dd74111ed3f3b8c837e435a01c13465a1f58
gongeek <gongweigm@gmail.com>
2015-01-26 12:39:22 +0800

    intent在actvity中的传递

--- src/com/example/studyAndroid/MyActivity.java
+++ src/com/example/studyAndroid/MyActivity.java
@@ -1,9 +1,14 @@
 package com.example.studyAndroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MyActivity extends Activity {
+    public final static String EXTRA_MESSAGE="com.example.studyAndroid";
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +16,12 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    public void sendMessage(View view){
+        EditText editText= (EditText) findViewById(R.id.edit_text);
+        String message=editText.getText().toString();
+        Intent intent=new Intent(this,DisplayMessageActivity.class);
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
     }
 }
